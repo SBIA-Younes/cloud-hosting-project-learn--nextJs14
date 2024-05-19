5964557d09d238c3de23b2f3078d7bcd2c0ba6c0
SBIA-Younes <brahim-sb@hotmail.fr>
2024-05-19 18:40:07 +0100

    API request caching management
    - feature: Added caching management for API requests
    - caching: Used next.revalidate option to reload data every 50 seconds
    - performance: Improved performance by caching API requests
    - options: Commented on different available caching options
    - doc: Documented caching management in Next.js

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -2,16 +2,27 @@ import ArticleItem from "@/components/articles/articleitem";
 import { Article } from "@/utils/types";
 
 const ArticlesPage = async () => {
-  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  // Fetch articles from the API
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts",
+    // { cache: "no-store" } // Do not use cache
+    // { cache: "force-cache" } // Use default cache
+    { next: { revalidate: 50 } } // Reload data every 50 seconds
+);
+
+  // Check if the response is successful
   if (!response.ok) {
-    throw new Error("Faild to fetch aerticles")
+    throw new Error("Faild to fetch aerticles");
   }
+
+  // Parse the response as an array of articles
   const articles: Article[] = await response.json();
 
   return (
     <section className="container m-auto px-5">
       <div className="flex items-center justify-center flex-wrap gap-7">
         {articles.map((item) => (
+          // Render an ArticleItem component for each article
           <ArticleItem article={item} key={item.id} />
         ))}
       </div>
